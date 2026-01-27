21e237739feb6ea033fdeff2a38e5be91a66f6ce
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-01-27 16:11:53 +0530

    Login Form / Signin Form

--- app/(auth)/login/login_form.tsx
+++ app/(auth)/login/login_form.tsx
@@ -1,15 +1,33 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { loginWithCredentials } from "@/lib/actions/user";
 import Link from "next/link";
 import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
 
 const LoginForm = () => {
+  const [data, action] = useFormState(loginWithCredentials, {
+    success: false,
+    message: "",
+  });
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending}>{pending ? "Logging in..." : "Login"}</Button>
+    );
+  };
+
   return (
     <div>
-      <form>
+      <form action={action}>
         <input type="email" placeholder="Email Id" name="email" required />
         <input type="password" placeholder="abc123@" name="password" required />
-        <Button>Submit</Button>
+        <SignInButton />
       </form>
+
+      {data && !data.success && <p className="text-red-600">{data.message}</p>}
+
       <p>
         Do not have account? <Link href="/register"> Register </Link>
       </p>
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import LoginForm from "./login_form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+  const session = await auth();
+  if (session) {
+    return redirect("/");
+  }
   return (
     <>
       <h1 className="text-lg">Login...</h1>
